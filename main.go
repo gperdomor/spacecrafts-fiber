1e004939dbfc3b5f89c9b5693b3c92f9f343c225
Gustavo Perdomo <gperdomor@gmail.com>
2025-07-10 01:32:28 -0400

    feat: added helmet, cors, error recover and healthchecks middlewares

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	app := fiber.New()
 
+	// Middlewares
+	app.Use(recover.New())
+	app.Use(helmet.New())
+	app.Use(cors.New())
+	app.Use(healthcheck.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"Title": "Hello, World!",
 		})
 	})
